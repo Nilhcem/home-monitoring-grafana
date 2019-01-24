4c355bbb2032c56b45b6af1edac706974d16b385
Gautier Nilhcem <nilhcem@gmail.com>
2019-01-24 22:22:19 +0100

    Use docker compose

--- 02-bridge/main.py
+++ 02-bridge/main.py
@@ -12,12 +12,12 @@ from typing import NamedTuple
 import paho.mqtt.client as mqtt
 from influxdb import InfluxDBClient
 
-INFLUXDB_ADDRESS = 'homeserver'
+INFLUXDB_ADDRESS = 'influxdb'
 INFLUXDB_USER = 'root'
 INFLUXDB_PASSWORD = 'root'
 INFLUXDB_DATABASE = 'home_db'
 
-MQTT_ADDRESS = 'homeserver'
+MQTT_ADDRESS = 'mosquitto'
 MQTT_USER = 'mqttuser'
 MQTT_PASSWORD = 'mqttpassword'
 MQTT_TOPIC = 'home/+/+'  # [bme280|mijia]/[temperature|humidity|battery|status]
